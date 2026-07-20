c4409747e36b11230d22c6c181d07e016aa73534
Marins <jmarinsti@gmail.com>
2026-07-20 15:14:05 -0300

    Criação do ProductRepositoryTests

--- src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -1,4 +1,26 @@
 package com.devsuperior.dscatalog.repositories;
 
+import com.devsuperior.dscatalog.entities.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
 public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists() {
+        long exintingId = 1L;
+
+        repository.deleteById(exintingId);
+
+        Optional<Product> result = repository.findById(exintingId);
+        Assertions.assertFalse(result.isPresent());
+    }
 }
